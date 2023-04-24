94cc336d0ad3b1967afba8ef1d1a8143d3db14e5
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-04-24 16:25:44 +0300

    All about loops

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,24 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+     
 	/* int initializes the number n*/
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
+     	int n;
+
+     	srand(time(0));
+     	n = rand() - RAND_MAX / 2;
+
+    	/* Check if the number is positive */
+    	if (n > 0)
+    	{
+        	printf("%d is positive\n", n);
+    	}
+    	/* Check if the number is negative */
+    	if (n < 0)
+    	{
+        	printf("%d is negative\n", n);
+    	}
+    	/* Check if the number is zero */
+    	if (n == 0)
+    	{
+        	printf("%d is zero\n", n);
+    	}
+
+    	return (0);
 }
