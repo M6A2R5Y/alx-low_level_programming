b34fbe098b926c097f540a0442a055385f28e71d
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-04-24 15:44:05 +0300

    All about loops

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,33 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/** 
- *main indicates beginning
- * 
- * Return 0 always indicates success
-*/
+
+/**
+ * main function - program entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	int n;	
-	/* Generates a random number */
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-        /* Check the last digit of n */
-	if ((n % 10) > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
-        {
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-        }
+    int n;
 
-	return (0);
+    /* Generates a random number */
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    /* Check the last digit of n */
+    if ((n % 10) > 5)
+    {
+        printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+    }
+    else if ((n % 10) == 0)
+    {
+        printf("Last digit of %d is %d and is 0\n", n, n % 10);
+    }
+    else if ((n % 10) < 6)
+    {
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+    }
+
+    return (0);
 }
