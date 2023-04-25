0a1e26e8481b456acb1dafa1a22ad9d57ce8acd9
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-04-25 18:30:12 +0300

    All about nested program codes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_sign prints the sign of a number
+ * print_sign - prints the sign of a number
  * n is the int to check
- * Returns 1 and prints + if n is greater than zero
+ * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
