d5ff751462ee38a4af7bc62355e3fe61b94ec7f2
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-04-26 22:55:15 +0300

    Debugging codes using rubber duck technique

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,42 +2,38 @@
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
- */
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month
+* @year: year
+* Return: void
+*/
 
 void print_remaining_days(int month, int day, int year)
 {
-    // Check if it's a leap year (divisible by 4 and not divisible by 100, except if divisible by 400)
-    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
-    {
-        // If it's a leap year and it's after February 29th, increment the day count
-        if (month >= 2 && day >= 60)
-        {
-            day++;
-        }
+    int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        // Print the day of the year and the remaining days
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
-    }
-    else
+    // Check if year is a leap year
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
     {
-        // If it's not a leap year or it's before February 29th
-        if (month == 2 && day == 60)
-        {
-            // If it's February 29th on a non-leap year, it's an invalid date
-            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-        }
-        else
-        {
-            // Print the day of the year and the remaining days (365 in a non-leap year)
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
+        days_in_month[2] = 29;  // February has 29 days in leap years
     }
+
+    // Check if date is valid
+    if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
+    {
+        printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+        return;
+    }
+
+    int day_of_year = 0;
+    for (int i = 1; i < month; i++)
+    {
+        day_of_year += days_in_month[i];
+    }
+    day_of_year += day;
+
+    printf("Day of the year: %d\n", day_of_year);
+    printf("Remaining days: %d\n", 365 + ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) - day_of_year);
 }
