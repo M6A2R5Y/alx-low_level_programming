7319b5b494ca2336a78b7626b984d929a51adb08
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-04-26 21:31:07 +0300

    Debugging codes using rubber duck technique

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Causes an infinite loop and avoids it
+* Return: 0
+*/
+int main(void)
+{
+    int i;
+
+    // print message warning of infinite loop
+    printf("Infinite loop incoming :(\n");
+
+    // initialize i to 0
+    i = 0;
+
+    // while loop commented out to avoid infinite loop
+    // while (i < 10)
+    // {
+    //     putchar(i);
+    // }
+
+    // print message indicating the infinite loop was avoided
+    printf("Infinite loop avoided! \\o/\n");
+
+    // return 0 to indicate successful program execution
+    return (0);
+}
