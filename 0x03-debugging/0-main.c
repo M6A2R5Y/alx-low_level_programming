7a560db0c11ded90fa78743293eb902901a9d103
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-04-26 21:19:37 +0300

    Debugging codes using rubber duck technique

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,12 +6,12 @@
  * Return: Always 0 Indicating Success
  */
 int main(void)
-{	
-    int i;
-    /* Initialize i to 0 */
-    i = 0;
-    /* Call the function with i as argument */
-    positive_or_negative(i);
-    /* Return 0 to indicate successful execution */
-    return (0);
+{
+	int i;
+        /* Initialize i to 0 */
+        i = 0;
+        /* Call the function with i as argument */
+        positive_or_negative(i);
+        /* Return 0 to indicate successful execution */
+        return (0);
 }
