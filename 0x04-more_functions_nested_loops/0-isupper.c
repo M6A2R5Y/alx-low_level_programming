76cdbf20bbfe9156c863eb491d0cf99e3b70ddf0
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-04-27 19:11:16 +0300

    C-functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,10 @@
-#include "main.h"
-
 /**
- * _isupper - checks for uppercase letters
- * @c: the character to be checked
+ * _isdigit - checks for a digit (0 through 9).
+ * @c: The character to be checked.
  *
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: 1 if c is a digit, 0 otherwise.
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
-	/* check if c is within the range of uppercase letter codes */
-	if (c >= 'A' && c <= 'Z')
-		{
-		return (1); /* c is uppercase, so return 1 */
-		}
-	else
-		{
-		return (0); /* c is not uppercase, so return 0 */
-		}
+	return (c >= '0' && c <= '9');
 }
