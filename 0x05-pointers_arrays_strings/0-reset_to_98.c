8b2cfd101c572181bc96fb33474678e41fb731d8
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-05-02 22:08:35 +0300

    Function that takes a pointer to an integer as its parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 updates the value of an integer to 98
+ * n is a pointer to an int to update
+ *
+ * Returns void
+ */
+void reset_to_98(int *n)
+{
+	/* dereference the pointerand update its value to 98 */
+	*n = 98;
+}
