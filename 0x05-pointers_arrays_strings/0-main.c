66445d010c18568cbe5eedac05d6c44e287b2be4
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-05-02 22:11:58 +0300

    all about pointers

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,18 +1,23 @@
-#include "main.h"
 #include <stdio.h>
 
-/**
- * main - check the code 
- *
- * Return: Always 0.
- */
+// Function prototype
+void reset_to_98(int *n);
+
 int main(void)
 {
     int n;
 
     n = 402;
     printf("n=%d\n", n);
+
+    // Call reset_to_98 function with a pointer to n
     reset_to_98(&n);
+
     printf("n=%d\n", n);
-    return (0);
+    return 0;
+}
+
+// Implementation of reset_to_98 function
+void reset_to_98(int *n) {
+    *n = 98;
 }
