e6d572efcb30905bc2d5f8f153c4990b4c7d9b58
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-05-02 23:51:01 +0300

    all about pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 	 /* find length of the string */
 	for (len = 0; str[len] != '\0';)
 	{
-		len++; 
+		len++;
 	}
 	/* determine starting index for printing second half of string */
 	if (len % 2 == 0)
