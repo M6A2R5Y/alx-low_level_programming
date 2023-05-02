87509af022f7db2a2960d772c74d878d2a53f2f9
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-05-02 23:00:21 +0300

    all about pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: is the string to print
+ */
+void print_rev(char *s)
+{
+	int len =0;
+	int i;
+	/* Calculat the length of the string */
+	while (s[len] != '\0')
+		len++;
+	/* Print the string in reverse */
+	for (i=len - 1; i>= 0; i--)
+		_putchar(s[i]);
+}
