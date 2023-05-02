571cac10880690f4e30c88d8d2210fcf371369cc
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-05-02 22:52:58 +0300

    all about pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,14 +8,14 @@
  */
 void _puts(char *str)
 {
-    /* While the current character is not a null terminator, write it to stdout */
-    while (*str != '\0')
-    {
-        /* Write the character to stdout */
-        write(STDOUT_FILENO, str, 1);
-        /* Move the pointer to the next character */
-        str++;
-    }
-    /* After the loop is finished, write a newline character to stdout */
-    write(STDOUT_FILENO, "\n", 1);
+	/* While current character is not a null terminator, write it to stdout */
+	while (*str != '\0')
+	{
+		/* Wriete the character to stdout */
+		write(STDOUT_FILENO, str, 1);
+		/* Move the pointer to the next character */
+		str++;
+	}
+	/* After the loop is finished, write a newline character to stdout */
+	write(STDOUT_FILENO, "\n", 1);
 }
