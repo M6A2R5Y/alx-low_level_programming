175b526b07abbcc20906aec1559623edc06c1b29
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-05-04 21:43:14 +0300

    pointers,arrays, and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,34 +1,31 @@
 #include "main.h"
 #include <stdio.h>
-char *rot13(char *str)
+/**
+ * rot13 - Encodes a string using rot13
+ * @s: pointer to string params
+ *
+ * Return: Pointer to the resulting encoded string.
+ */
+char *rot13(char *s)
 {
 	int i, j;
-	char c;
-	/* Loop through each character in the string */
-	for (i = 0; str[i] != '\0'; i++)
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	/* Loop through the string */
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		c = str[i];
-		/* Check if the character is a letter */
-		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		/* Lopp through the rot13 mapping arrays */
+		for (j = 0; j < 52; j++)
 		{
-			/* Shift the letter by 13 positions */
-			for (j = 0; j < 13; j++)
+			/* If the current character matches with a letter */
+			if (s[i] == data1[j])
 			{
-				c++;
-				/* If the letter goes beyond 'z' or 'Z', wrap it around */
-				if (c > 'z')
-				{
-					c = 'a';
-				}
-				if (c > 'Z' && c < 'a')
-				{
-					c = 'A';
-				}
+				/* Replace it with the corresponding rot13 letter */
+				s[i] = datarot[j];
+				break;
 			}
 		}
-		/* Replace the character in the string with the encoded character */
-		str[i] = c;
 	}
-	/* Return the encoded string */
-	return str;
+	/* return the encoded string */
+	return (s);
 }
