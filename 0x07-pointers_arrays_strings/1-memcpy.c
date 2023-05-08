edc504cc3ceba0f1a0fd54f7ed74c15664ff7f9e
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-05-09 00:01:43 +0300

    more on pointers,arrays, and strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,18 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _memcpy - Copies n bytes from memory area src to memory area dest
+ * @dest: Destination memory area
+ * @src: Source memory area
+ * @n: Number of bytes to copy
+ *
+ * Return: Pointer to destination memory area
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i;
 	/* Loop through the first n bytes of the src memory area */
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		/* Copy the byte at the current index i from src to dest */
 		dest[i] = src[i];
