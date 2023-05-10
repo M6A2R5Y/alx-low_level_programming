a60506abf11e23a2d9a75a0fd41df980d16d85ba
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-05-10 22:41:37 +0300

    C - Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,23 +5,23 @@ int calculate_sqrt(int n, int guess)
 	if (guess * guess == n)
 	{
 		/* Base case: found the square root */
-		return guess;
+		return (guess);
 	}
 	if (guess * guess > n)
 	{
 		/* Base case: no natural square root */
-		return -1;
+		return (-1);
 	}
 	/* Recursive call with the next guess */
-	return calculate_sqrt(n, guess + 1);
+	return (calculate_sqrt(n, guess + 1));
 }
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
 		/* Error case: negative number */
-		return -1;
+		return (-1);
 	}
 	/* Start with guess = 0 */
-	return calculate_sqrt(n, 0);
+	return (calculate_sqrt(n, 0));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,22 +5,23 @@ int is_divisible(int n, int divisor)
 	if (divisor == 1)
 	{
 		/* Base case: reached 1, not prime */
-		return 0;
+		return (0);
 	}
 	if (n % divisor == 0)
 	{
 		/* Base case: found a divisor, not prime */
-		return 1;
+		return (1);
 	}
 	/* Recursive call with the next divisor */
-	return is_divisible(n, divisor - 1);
+	return (is_divisible(n, divisor - 1));
 }
 int is_prime_number(int n)
 {
 	if (n <= 1)
 	{
 		/* Base case: numbers less than or equal to 1 are not prime */
-		return 0;
+		return (0);
 	}
 	/* Check divisibility starting from n-1 */
+	return (actual_prime(n, i - 1));
 }
