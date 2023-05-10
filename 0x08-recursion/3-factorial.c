a794059cf081388a08c0c032a9777d7f10a62e56
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-05-10 22:31:57 +0300

    C - Recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,13 +5,13 @@ int factorial(int n)
 	if (n < 0)
 	{
 		/*  Error case: negative number */
-		return -1;
+		return (-1);
 	}
 	if (n == 0)
 	{
 		/* Base case: factorial of 0 is 1 */
-		return 1;
+		return (1);
 	}
 	/* Recursive call with n-1 */
-	return n * factorial(n - 1);
+	return (n * factorial(n - 1));
 }
