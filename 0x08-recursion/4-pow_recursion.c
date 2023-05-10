e3562c789233acee0273f457eb79f10edd6d4e56
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-05-10 22:34:54 +0300

    C - Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,13 +5,13 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 	{
 		/* Error case: negative exponent */
-		return -1;
+		return (-1);
 	}
 	if (y == 0)
 	{
 		/* Base case: any number raised to the power of 0 is 1 */
-		return 1;
+		return (1);
 	}
 	/* Recursive call with y-1 */
-	return x * _pow_recursion(x, y - 1);
+	return (x * _pow_recursion(x, y - 1));
 }
