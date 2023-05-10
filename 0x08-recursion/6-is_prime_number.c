9555d4055510e4f7ffe98518b8ecff85157e3d4d
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-05-10 22:47:08 +0300

    C - Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,20 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-int is_divisible(int n, int divisor)
-{
-	if (divisor == 1)
-	{
-		/* Base case: reached 1, not prime */
-		return (0);
-	}
-	if (n % divisor == 0)
-	{
-		/* Base case: found a divisor, not prime */
-		return (1);
-	}
-	/* Recursive call with the next divisor */
-	return (is_divisible(n, divisor - 1));
-}
+int actual_prime(int n, int i);
+/**
+ * is_prime_number - Determines if an integer is a prime number.
+ * @n: Number to evaluate.
+ *
+ * Return: 1 if n is a prime number, 0 if not.
+ */
 int is_prime_number(int n)
 {
 	if (n <= 1)
@@ -24,4 +16,24 @@ int is_prime_number(int n)
 	}
 	/* Check divisibility starting from n-1 */
 	return (actual_prime(n, i - 1));
+}
+/**
+ * actual_prime - Helper function to recursively check if a number is prime.
+ * @n: Number to evaluate.
+ * @i: Iterator.
+ *
+ * Return: 1 if n is prime, 0 if not.
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+	{
+		return 1;
+	}
+	/* Check if n is divisible by i, and i is greater than 0. */
+	if (n % i == 0 && i > 0)
+	{
+		return 0;
+	}
+	return actual_prime(n, i - 1);
 }
