88cd538c3d50f5db874adc36eb3b6d3a5454c62a
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-05-23 15:21:52 +0300

    programs on structures and typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,30 +1,24 @@
 #include <stdlib.h>
 #include "dog.h"
 
-/**
- * init_dog - Initializes a variable of type struct dog.
- * @d: Pointer to struct dog to initialize.
- * @name: Name to initialize.
- * @age: Age to initialize.
- * @owner: Owner to initialize.
+/*
+ * This function initializes a variable of type struct dog.
  *
- * Description: This function initializes a struct dog variable with the provided values.
- * If the pointer to struct dog is NULL, it allocates memory for a new struct dog using malloc.
- * The name, age, and owner fields of the struct dog are assigned the provided values.
- *Return: void
+ * Parameters:
+ *
+ * d: pointer to struct dog to initialize
+ * name: name to initialize
+ * age: age to initialize
+ * owner: owner to initialize
  * 
+ * Returns: void
+ *
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	/* Check if d is NULL. If it is, allocate memory for a new struct dog. */
 	if (d == NULL)
 	{
-		d = malloc(sizeof(struct dog));
-		if (d == NULL)
-		{
-			/* Could not allocate memory. */
-			return;
-		}
+		return;
 	}
 	/* Set the name, age, and owner members of the struct dog. */
 	d->name = name;
