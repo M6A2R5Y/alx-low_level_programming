7b84f630f122c4f65df5e7cc1c0875eb08c1d782
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-05-23 16:30:56 +0300

    programs on structures and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,32 +7,41 @@
  * @owner: owner of the new dog
  * Return: pointer to the newly created dog struct
  */
+/*
+ * Get the lengths of the name and owner strings.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	/* Check if name and owner are NULL. If they are, return NULL. */
-	if (name == NULL || owner == NULL)
+	int len1 = _strlen(name);
+	int len2 = _strlen(owner);
+	/* Allocate memory for a new dog structure. */
+	dog_t *dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
 	{
+		/* Failed to allocate memory, return NULL. */
 		return (NULL);
 	}
-	/* Allocate memory for a new dog. */
-	dog_t *newDog = malloc(sizeof(dog_t));
-	if (newDog == NULL)
+	/* Allocate memory for the name and owner strings. */
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
 	{
+		/* Failed to allocate memory for name, free dog and return NULL. */
+		free(dog);
 		return (NULL);
 	}
-	int nameLen = strlen(name);
-	int ownerLen = strlen(owner);
-	newDog->name = malloc(sizeof(char) * (nameLen + 1));
-	newDog->owner = malloc(sizeof(char) * (ownerLen + 1));
-	if (newDog->name == NULL || newDog->owner == NULL)
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
 	{
-		free(newDog->name);
-		free(newDog->owner);
-		free(newDog);
+		/* Failed to allocate memory for owner, free dog, name and return NULL. */
+		free(dog->name);
+		free(dog);
 		return (NULL);
 	}
-	strncpy(newDog->name, name, nameLen + 1);
-	strncpy(newDog->owner, owner, ownerLen + 1);
-	newDog->age = age;
-	return newDog;
+	/*Copy the name and owner strings into the dog structure. */
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	/* Set the age. */
+	dog->age = age;
+	/* Return the new dog. */
+	return dog;
 }
