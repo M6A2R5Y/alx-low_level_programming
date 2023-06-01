96dba02033cac9237ef259c0f46ef8a334726db9
MaryKinyanjui <marya.kinyanjui@gmail.com>
2023-06-01 13:49:35 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,39 +1,56 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-
-int main(int argc, char *argv[]) 
-{
-  int sum = sum_them_all(argc - 1, &argv[1]);
-  printf("The sum is: %d\n", sum);
-  return 0;
-}
-
 /*
- * sum_them_all - Calculates the sum of all its parameters.
- * @n: The number of parameters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
+ * sum_them_all()
  *
- * Return: If @n is 0, the function returns 0.
- *         Otherwise, it returns the sum of all parameters.
+ * This function takes a variable number of arguments and returns the sum of all of them.
+ *
+ * Parameters:
+ *   n - The number of arguments
+ *
+ * Returns:
+ *   The sum of all the arguments, or 0 if n is 0
  */
 
-int sum_them_all(const unsigned int n, ...)
-{
-	va_list nums;
-	int sum = 0;
-	unsigned int i;
-	if (n == 0)
-	{
-		return (0);
-	}
-	va_start(nums, n);
-	/* Iterate over the variadic arguments and add them to the sum. */
-	for ( i = 0; i < n; i++)
-	{
-		sum += va_arg(nums, int);
-	}
-	/* Use the va_end macro to destroy the va_list object. */
-	va_end(nums);
-	/* Return the sum. */
-	return (sum);
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...) {
+  /*
+   * va_list - A variable argument list
+   * sum - The sum of all the arguments
+   * i - A counter
+   */
+  va_list list;
+  int sum = 0;
+  unsigned int i;
+
+  /*
+   * Initialize the va_list variable with the number of arguments
+   */
+  va_start(list, n);
+
+  /*
+   * Iterate over all the arguments and add them to the sum
+   */
+  for (i = 0; i < n; i++) {
+    /*
+     * Get the next argument from the list
+     */
+    int arg = va_arg(list, int);
+
+    /*
+     * Add the argument to the sum
+     */
+    sum += arg;
+  }
+
+  /*
+   * Terminate the use of the va_list variable
+   */
+  va_end(list);
+
+  /*
+   * Return the sum
+   */
+  return sum;
 }
