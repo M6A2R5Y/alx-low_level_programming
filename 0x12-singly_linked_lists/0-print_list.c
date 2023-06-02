9aa52cfe245e1b1c9127826c35330232ab3eb054
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-06-02 13:54:54 +0300

    iterating through the linked list starting from the head printing data of each

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,19 +4,23 @@ size_t print_list(const list_t *h)
 {
 	/* variable to count the number of nodes*/
 	size_t count = 0;
+	/* iterate through the linked list until h becomes NULL*/
 	while (h != NULL)
 	{
 		if (h->str != NULL)
 		{
-			printf("[%zu] %s\n", count++, h->str);
+			printf("[0] (nil)\n";
 		}
 		else
 		{
-			printf("[%zu] (nil)\n");
+		        /* print the length and the string*/
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		
 		/* move to the next node*/
 		h = h->next;
+		/* Increment of count of nodes printed*/
+		count++;
 	}
 	return(count);
 }
